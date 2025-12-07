ba8f22f22e7163983789b97dee5432211d820db9
Riley Wheadon <rileywheadon@gmail.com>
2025-12-07 09:43:21 -0800

    Remove production boilerplate.

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -121,12 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-# For production deployment only
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
